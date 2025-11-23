d7ea54dc12f9cfba74462719f80c05531a45e63a
Harry7986 <emarh.harry.code@gmail.com>
2025-11-23 23:45:50 +0100

    Error Middleware

---
+++ src/core/errors/error.middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "./AppError";
+import { ZodError } from "zod";
+
+export function errorMiddleware(err: any, req: Request, res: Response, next: NextFunction) {
+    console.log(err);
+
+    if (err instanceof AppError) {
+        return res.status(err.status).json({ error: err.message });
+    }
+
+    if (err instanceof ZodError) {
+        return res.status(400).json({ error: err.issues });
+    }
+    
+}

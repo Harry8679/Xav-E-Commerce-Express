735957d36ea636588bc4f89cdc7d220f0871cbde
Harry7986 <emarh.harry.code@gmail.com>
2025-11-24 16:25:43 +0100

    Validate 2

--- src/core/utils/validate.ts
+++ src/core/utils/validate.ts
@@ -1,1 +1,7 @@
 import { ZodSchema } from "zod";
+
+export function validate<T>(schema: ZodSchema<T>, data: unknown) {
+    const parsed = schema.safeParse(data);
+    if (!parsed.success) throw parsed.error;
+    return parsed.data;
+}

9f45f2cb648b860761d30606d91231b662c24c79
Harry7986 <emarh.harry.code@gmail.com>
2025-11-24 18:12:05 +0100

    Auth Service

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { AppError } from "../../core/errors/AppError";
+import { User } from "./auth.model";
+import { RegisterInput } from "./auth.schema";
+import argon2 from "argon2";
+
+export class AuthService {
+    async register(dto: RegisterInput) {
+        const existing = await User.findOne({ email: dto.email });
+        if (existing) throw new AppError("Email already registred", 400);
+
+        const hashedPassword = await argon2.hash(dto.password);
+
+        const user = await User.create({
+            firstName: dto.firstName,
+            lastName: dto.lastName,
+            email: dto.email.toLowerCase(),
+            password: hashedPassword
+        });
+
+        return { id: user._id, email: user.email };
+    }
+}

62884fd876a1167abcb261eebbadccbe2b9bdcaa
Harry7986 <emarh.harry.code@gmail.com>
2025-11-24 16:35:08 +0100

    User Model 2

--- src/modules/auth/auth.model.ts
+++ src/modules/auth/auth.model.ts
@@ -5,4 +5,6 @@ const userSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true }
-});
+}, { timestamps: true });
+
+export const User = mongoose.model("User", userSchema);

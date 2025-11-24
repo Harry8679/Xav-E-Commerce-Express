dac4d8b20e8253d46489d5c5046393ba41f92d0e
Harry7986 <emarh.harry.code@gmail.com>
2025-11-24 21:49:49 +0100

    App ts 2

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./modules/auth/auth.routes";
+import { errorMiddleware } from "./core/errors/error.middleware";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/v1/auth", authRoutes);
+
+// Error
+app.use(errorMiddleware);
+
+export default app;

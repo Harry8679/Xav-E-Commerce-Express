9540b70fd42e6eb560f2a7ea0827c04d8c5619fb
Harry7986 <emarh.harry.code@gmail.com>
2025-11-24 21:17:27 +0100

    Auth Controller

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { validate } from "../../core/utils/validate";
+import { registerSchema } from "./auth.schema";
+import { authService } from "./auth.service";
+
+export class AuthController {
+    async register(req: Request, res: Response, next: NextFunction) {
+        try {
+            const data = validate(registerSchema, req.body);
+            const user = await authService.register(data);
+            return res.status(201).json({ user });
+        } catch (err) {
+            next(err);
+        }
+    }
+};

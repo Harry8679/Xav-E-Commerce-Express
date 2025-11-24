40384e6d4ef3be59424aceaca46a5f3cb9c36681
Harry7986 <emarh.harry.code@gmail.com>
2025-11-24 22:37:15 +0100

    Server

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import app from "./app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+async function start() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI as string);
+        console.log("MONGO DB Connected !");
+
+        app.listen(`Server is running on the port ${PORT}`);
+    } catch(error) {
+        console.error("Error connecting to server:", error);
+    }
+}

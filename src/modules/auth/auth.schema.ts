aca46a35c7bc4b2cb28245119cc9523c66c2e34e
Harry7986 <emarh.harry.code@gmail.com>
2025-11-25 06:43:46 +0100

    Login Schema 3

--- src/modules/auth/auth.schema.ts
+++ src/modules/auth/auth.schema.ts
@@ -13,6 +13,7 @@ export const loginSchema = z.object({
 });
 
 export type RegisterInput = z.infer<typeof registerSchema>;
+export type LoginInput = z.infer<typeof loginSchema>;
 
 /*
 Cette ligne :

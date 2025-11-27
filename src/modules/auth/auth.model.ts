e036d5c74afb976945d6d230be525925606a01f8
Harry7986 <emarh.harry.code@gmail.com>
2025-11-27 15:44:41 +0100

    Auth Model

--- src/modules/auth/auth.model.ts
+++ src/modules/auth/auth.model.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import { IUSer } from "./auth.types";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema<IUSer>({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true, lowercase: true },

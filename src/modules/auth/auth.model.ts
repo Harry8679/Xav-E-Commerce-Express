ae52b545197556b26e747ab72819914423e4c24b
Harry7986 <emarh.harry.code@gmail.com>
2025-11-27 15:51:57 +0100

    Auth Model 3

--- src/modules/auth/auth.model.ts
+++ src/modules/auth/auth.model.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import { IUSer } from "./auth.types";
+import { IUser } from "./auth.types";
 
-const userSchema = new mongoose.Schema<IUSer>({
+const userSchema = new mongoose.Schema<IUser>({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true }
 }, { timestamps: true });
 
-export const User = mongoose.model<IUSer>("User", userSchema);
+export const User = mongoose.model<IUser>("User", userSchema);

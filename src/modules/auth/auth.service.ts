86b73707578acf12615cf91ca8629f6cc81925a5
Harry7986 <emarh.harry.code@gmail.com>
2025-11-27 16:03:08 +0100

    Auth Service 6

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -29,7 +29,18 @@ export class AuthService {
         const passwordValid = await argon2.verify(user.password, dto.password);
         if (!passwordValid) throw new AppError("Invalid Credentials", 400);
 
-        const accessToken = jwt
+        const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET as string, { expiresIn: "15m" });
+        const refreshToken = jwt.sign({ userId: user._id }, process.env.JWT_REFRESH_SECRET as string, { expiresIn: "7d" });
+
+        return {
+            user: {
+                id: user._id,
+                email: user.email,
+            },
+            accessToken,
+            refreshToken,
+        };
+  }
     }
 }
 

f282da712374d6bb63c18fbed7c1bdd8238b1191
Harry7986 <emarh.harry.code@gmail.com>
2025-11-28 19:44:18 +0100

    Login Controller 2

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { validate } from "../../core/utils/validate";
-import { registerSchema } from "./auth.schema";
+import { loginSchema, registerSchema } from "./auth.schema";
 import { authService } from "./auth.service";
 
 export class AuthController {
@@ -14,7 +14,19 @@ export class AuthController {
         }
     }
 
-    async login(req: Request, res: Response, next: NextFunction) {}
+    async login(req: Request, res: Response, next: NextFunction) {
+        try {
+            const data = validate(loginSchema, req.body);
+            const result = await authService.login(data);
+
+            res.cookie("refreshToken", result.refreshToken, {
+                httpOnly: true,
+                secure: false,
+                sameSite: "strict",
+                maxAge: 7 * 24 * 60 * 60 * 1000,
+            });
+        } catch(err) {}
+    }
 };
 
 export const authController = new AuthController();
